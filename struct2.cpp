d13c21611f1beb23840427c33b44a21a6a684801
Pawel <dmochowski@avenus.pl>
2019-10-29 13:43:20 +0100

    Dynamically allocated structure

---
+++ struct2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+struct informacje
+{
+	std::string firma;
+	int srednica;
+	float waga;
+	
+};
+
+int main(void)
+{
+
+using namespace std;
+informacje * pz = new informacje; //alokujemy dynamicznie strukturę
+ 
+
+cout << "What is the company name?" << endl;
+cin >> (*pz).firma; // mozna rowniez pz->firma
+cout << "What is the diameter of a pizza?" << endl;
+cin >> pz->srednica; // mozna rowniez *(pz).srednica
+cout << "What is the weight of a pizza?" << endl;
+cin >> pz->waga;
+
+cout << "OK, so company name is: " << pz->firma << ", diameter of pizza " << pz->srednica << " and weight of pizza is " << pz->waga << endl;
+
+//zwolnienie pamięci
+delete pz;
+
+
+return 0;
+}
+	
