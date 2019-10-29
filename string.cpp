e69ee19f9861bd1df7403fcacd6e771bf69320cd
Pawel <dmochowski@avenus.pl>
2019-10-29 12:56:53 +0100

    cin.getline(), cin.get() and  string

---
+++ string.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+	
+	using namespace std;
+	string imie, nazwisko;
+	short ocena,wiek;
+
+	cout << "What is your name?" << endl;
+	//cin.get(imie,15).get(); //wczytanie wiersza i znaku nowego wiersza
+	cin >> imie;
+	cout << "What is your surname?" << endl;
+	//cin.getline(nazwisko,20);
+	cin >> nazwisko;
+	cout << "How do you rate from 1 to 6" << endl;
+	cin >> ocena;
+	cout << "How old are you?" << endl;
+	cin >> wiek;
+
+	cout << "Nazywasz się: " << imie << " "<< nazwisko << endl;
+	cout << "Twoje ocena to: " << ocena-1 << endl;
+	cout << "Wiek: " << wiek << endl;
+
+ 
+
+return 0;
+}
