81d6a8e0ec35aaff3be3af8be0f0798ab0e91d72
prapdm <dmochowski@avenus.pl>
2019-11-14 21:20:36 +0100

    Struct and pointer

--- struct2.cpp
+++ struct2.cpp
@@ -14,7 +14,6 @@ int main(void)
 using namespace std;
 informacje * pz = new informacje; //alokujemy dynamicznie strukturę w pamięci
  
-
 cout << "What is the company name?" << endl;
 cin >> (*pz).firma; // mozna rowniez pz->firma
 cout << "What is the diameter of a pizza?" << endl;
@@ -23,7 +22,7 @@ cout << "What is the weight of a pizza?" << endl;
 cin >> pz->waga;
 
 cout << "OK, so company name is: " << pz->firma << ", diameter of pizza " << pz->srednica << " and weight of pizza is " << pz->waga << endl;
-
+ 
 //zwolnienie pamięci
 delete pz;
 
