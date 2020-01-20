fc9b99e86b89374f55dfc69f097b415ea1e5b2b4
prapdm <dmochowski@avenus.pl>
2020-01-20 12:44:07 +0100

    Chapter 9 exercises

---
+++ Chapter9/exercise_2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+const int ArSize = 10;
+
+
+
+//prototypy
+void strcount(const std::string * str);
+
+int main(void)
+{
+
+	using namespace std;
+	string input;
+	static string * pi = &input;
+ 
+	cout << "Enter text: " << endl;
+	getline(cin, input);
+	
+	while(input != "")
+	{
+ 		
+ 		
+		strcount(pi);
+		cout << "Enter next line (empty line end fun): " << endl;
+		getline(cin, input);
+
+	}
+
+	return 0;
+}
+
+void strcount(const std::string * str)
+{
+
+	using namespace std;
+	static int total = 0; //statyczna zmienna lokalna podtrzymywana w pamieci rowniez wtedy kiedy program nie wykonuje instrukcji bloku
+	int count = 0;
+
+	cout << "\"" << *str << "\" has ";
+	count +=  (*str).size();
+	total += count;
+	cout << count << " characters\n";
+	cout << "Total "<< total << " characters\n" ;
+
+}
