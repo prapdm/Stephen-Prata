ab7117231987251f6071b15a45c3a2190cf02136
prapdm <dmochowski@avenus.pl>
2020-04-07 11:31:21 +0200

    Vector random walk

---
+++ randomwalk.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<cstdlib> //for rand() and srand()
+#include<ctime> //for time()
+#include "vect.h"
+
+
+int main()
+{
+	using namespace std;
+	using VECTOR::Vector;
+	srand(time(0)); //srand umozliwia ustawienia wlasnego zarodka time(0) zwieksza przypadkowosc
+	double direction;
+	Vector step;
+	Vector result(0.0, 0.0);
+	unsigned long steps = 0;
+	double target;
+	double dstep;
+	cout << "Enter distance (type e to end): ";
+
+	while(cin >> target)
+	{
+		cout << "Enter step distance: ";
+		if(!(cin >> dstep)) break;
+
+		while(result.magval() < target)
+		{
+			direction = rand() % 360;
+			step.reset(dstep, direction, Vector::POL);
+			result = result + step;
+			steps++;
+		}
+
+		cout << "After " << steps << " steps you reach position:\n" ;
+		cout << result << endl;
+		result.polar_mode();
+		cout << "so " << result << endl;
+		cout << "Avarge step length: " << result.magval()/steps << endl;
+		steps = 0;
+		result.reset(0.0, 0.0) ;
+		cout << "Enter distance (type e to end): ";
+	}
+	cout << "Bye bye\n";
+	cin.clear();
+	while(cin.get() != '\n') continue;
+
+
+	return 0;
+}
